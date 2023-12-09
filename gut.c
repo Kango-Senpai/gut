c7a3d3841be0acf7c1e8b988ec6a97b3154ed593
Kango-Senpai <76138220+Kango-Senpai@users.noreply.github.com>
2023-12-09 00:47:22 -0600

    Dev1 (#4)
    * Changes.
    
    * gut mimics cat's functionality when no arguments are provided. (Echos to stdin).
    
    * Added flag parsing functionality.
    
    * Wrote code for stream processing. Needs testing.
    
    * Minor adjustments and bug fixes. Basic functionality of gutting a file written. Default number of lines to fetch above and below is five.
    
    * Renamed 'main.c' to 'gut.c'.
    
    * Added help screen.
    
    * Help menu changes.
    
    * Added chunk mode.
    
    * Added proper handling of '-h' flag.
    
    * Added flags to getopt loop.
    
    * Light bug fix.
    
    * Added 'bin/' directory.

--- gut.c
+++ gut.c
@@ -6,6 +6,10 @@
 #include <getopt.h>
 
 #include <ctype.h>
+#include <stdbool.h>
+#define PROGRAM_NAME "gut"
+#define FILE_NAME "numbers.txt"
+
 #define PROGRAM_NAME "gut"
 #define FILE_NAME "numbers.txt"
 
@@ -18,7 +22,7 @@ void copy_stream(FILE *src, FILE *dst){
     return;
 }
 
-void process_stream(FILE *stream, int li, int tl, int hl){// line_index, tail_lines, head_lines
+void process_stream(FILE *stream, int li, int tl, int hl){// line_index, tail_lines, header_lines
     int head_line = li - hl;
     int tail_line = li + tl;
     char c;
@@ -37,6 +41,43 @@ void process_stream(FILE *stream, int li, int tl, int hl){// line_index, tail_li
     return;
 }
 
+void process_chunk(FILE *stream, int li, int tl, int hl) {
+    char c, last_c;
+    int header_line = li-hl;
+    int lines = 1;
+    while ((c = getc(stream)) != EOF){
+        if (lines >= header_line){
+            putc(c,stdout);
+        }
+        if (c == '\n'){
+            lines++;
+        }
+        if(c == '\n' && last_c == '\n' && lines > li){
+            break;
+        }
+        last_c = c;
+    }
+    return;
+}
+
+void print_help(){
+    puts(
+        "Usage: gut [OPTION]... [FILE]...\n"
+        "Print all lines of FILE or chunks of FILE.\n"
+        "\n"
+        "With no FILE, or when FILE is -, read standard input.\n"
+        "With no OPTION gut outputs the first five lines to stdout.\n"
+        "-n NUM		set the line index to NUM\n"
+        "-u NUM		read NUM lines above the index\n"
+        "-d NUM         read NUM lines below the index\n"
+        "-c         chunk mode. Read until the next empty line.\n"
+        "\n"
+        "https://github.com/Kango-Senpai/gut"
+    );
+
+    return;
+}
+
 
 int main(int argc, char **argv){
     if (argc <= 1){
@@ -44,14 +85,15 @@ int main(int argc, char **argv){
         copy_stream(stdin,stdout);
         return 0;
     }
+    void (*process_function_ptr)(FILE*,int,int,int) = &process_stream; //Set the default processing method. This will change if '-c' is passed.
     //Program options. Will later be moved to a struct. stuct line_options???
     int line_index = 0, head_lines = 5, tail_lines = 5;
-    char c;
+    char *c = malloc(sizeof(char));
 
     opterr = 0;
     //Get command line flags and set program options.
-    while ((c = getopt(argc,argv,"d:n:u:")) != EOF){
-        switch (c){
+    while ((*c = getopt(argc,argv,"cd:hn:u:")) != EOF){
+        switch (*c){
             case 'd':
                 tail_lines = atoi(optarg);
                 break;
@@ -62,12 +104,21 @@ int main(int argc, char **argv){
             case 'u':
                 head_lines = atoi(optarg);
                 break;
+            case 'c':
+                process_function_ptr = &process_chunk;
+                break;
+            case 'h':
+                print_help();
+                free(c);
+                return 0;
             case '?':
                 if (optopt == 'n'){
                     fprintf(stderr,"Option '-%c' requires an argument.\n",optopt);
                 }
                 else if (isprint(optopt)){
                     fprintf(stderr,"Unknown option '-%c'.\n",optopt);
+                    print_help();
+
                 }
                 else {
                     fprintf(stderr,"Arguments contained an invalid character. ('\\x%x')\n",optopt);
@@ -75,6 +126,7 @@ int main(int argc, char **argv){
                 return 1;
         }
     }
+    free(c);
     //Non-option arguments shall be treated as files.
     FILE *stream = NULL;
     //Try to parse remaining arguments as file names. If the stream is never set, it is assigned stdin.
@@ -85,11 +137,12 @@ int main(int argc, char **argv){
             printf("Unable to read file: %s\n",argv[optind]);
             continue;
         }
-        process_stream(stream,line_index,tail_lines,head_lines);
+        (*process_function_ptr)(stream,line_index,tail_lines,head_lines);
     }
     if (stream == NULL){
         stream = stdin;
-        process_stream(stream,line_index,tail_lines,head_lines);
+        (*process_function_ptr)(stream,line_index,tail_lines,head_lines);
+
     }
     
     // If argc is 2, user provided either a flag or a file.
